fb8636fc641361d147a18afd25a8d8feb38c12b0
AmirHosseinAkb <amirbvk1@gmail.com>
2022-03-07 15:27:29 +0330

    ConfirmPrimaryConfigs

--- AntomiShop/Program.cs
+++ AntomiShop/Program.cs
@@ -21,5 +21,9 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
