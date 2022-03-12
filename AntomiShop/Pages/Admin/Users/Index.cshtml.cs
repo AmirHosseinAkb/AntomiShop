098666486d25268556c3ae9e6a6b0423fdc0013a
AmirHosseinAkb <amirbvk1@gmail.com>
2022-03-13 00:35:32 +0330

    ConfirmIndex

--- AntomiShop/Pages/Admin/Users/Index.cshtml.cs
+++ AntomiShop/Pages/Admin/Users/Index.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Antomi.Core.Services.Interfaces;
+using Antomi.Core.DTOs.User;
 
 namespace AntomiShop.Pages.Admin.Users
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private IUserService _userService;
+        public IndexModel(IUserService userService)
         {
+            _userService = userService;
+        }
+        public ShowUsersInAdminViewModel ShowUsersInAdminViewModel { get; set; }
+        public void OnGet(int pageId=1,string filterName="",string filterEmail="")
+        {
+            ShowUsersInAdminViewModel=_userService.GetUsersForShowInAdmin(pageId,filterName,filterEmail);
         }
     }
 }
