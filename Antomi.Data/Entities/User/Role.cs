6b8a16ca5c8d8f3a581dc7017300d404fb5a2341
AmirHosseinAkb <amirbvk1@gmail.com>
2022-03-16 22:28:51 +0330

    OptionalRoleProperties

--- Antomi.Data/Entities/User/Role.cs
+++ Antomi.Data/Entities/User/Role.cs
@@ -20,8 +20,8 @@ namespace Antomi.Data.Entities.User
         public bool IsDeleted { get; set; }
 
         #region Relations
-        public List<RolePermission> RolePermissions { get; set; }
-        public List<User> Users { get; set; }
+        public List<RolePermission>? RolePermissions { get; set; }
+        public List<User>? Users { get; set; }
         #endregion
     }
 }
